e45a8f9697b534d44d8f066d1d38c7be24494740
Benoît Rognier <benoit.rognier@edulera.com>
2020-12-06 09:03:19 +0100

    Updates

---
+++ precache-manifest.372efdcf4d1589cadcf201ac7d94fabe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f3942ea359a8a1e0468be3f06868e3",
+    "url": "/completium-dapp-ideabox/index.html"
+  },
+  {
+    "revision": "d45e9d3810dffd532caf",
+    "url": "/completium-dapp-ideabox/static/css/main.5533175d.chunk.css"
+  },
+  {
+    "revision": "5fa2f50fb30cf8e791c9",
+    "url": "/completium-dapp-ideabox/static/js/2.5f676bfe.chunk.js"
+  },
+  {
+    "revision": "e7fb2d165a507a6dddc5e0b7e1396723",
+    "url": "/completium-dapp-ideabox/static/js/2.5f676bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45e9d3810dffd532caf",
+    "url": "/completium-dapp-ideabox/static/js/main.746fe9dc.chunk.js"
+  },
+  {
+    "revision": "25b23a1464cf0642ff00",
+    "url": "/completium-dapp-ideabox/static/js/runtime-main.085e7e1c.js"
+  }
+]);
