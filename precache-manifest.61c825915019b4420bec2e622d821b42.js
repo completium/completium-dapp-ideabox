82ec5c3129e7dbc5b2ccd0b84cf0c878ed8b70d3
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-01-14 17:02:39 +0100

    Updates

---
+++ precache-manifest.61c825915019b4420bec2e622d821b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a2f70ec9fc68c162d15bb13c99e92c",
+    "url": "/completium-dapp-ideabox/index.html"
+  },
+  {
+    "revision": "99f2a55b06569bc02bee",
+    "url": "/completium-dapp-ideabox/static/css/main.5533175d.chunk.css"
+  },
+  {
+    "revision": "389b15b2c0863e305a40",
+    "url": "/completium-dapp-ideabox/static/js/2.b12a90ec.chunk.js"
+  },
+  {
+    "revision": "e99e785e7f1297ef036da972610c4827",
+    "url": "/completium-dapp-ideabox/static/js/2.b12a90ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f2a55b06569bc02bee",
+    "url": "/completium-dapp-ideabox/static/js/main.3e961392.chunk.js"
+  },
+  {
+    "revision": "25b23a1464cf0642ff00",
+    "url": "/completium-dapp-ideabox/static/js/runtime-main.085e7e1c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/completium-dapp-ideabox/precache-manifest.a48469031b07a8962756a7f57b5448dd.js"
+  "/completium-dapp-ideabox/precache-manifest.61c825915019b4420bec2e622d821b42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/completium-dapp-ideabox/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
